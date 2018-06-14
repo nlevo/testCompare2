6468435c434bb165afc365e0045a10217a0031aa
Nicolas Levo <nikola.levo@gmail.com>
2018-06-14 14:30:18 -0400

    Add chainparamsbase.cpp dash

--- chainparamsbase.cpp
+++ chainparamsbase.cpp
@@ -1,118 +1,62 @@
-// Copyright (c) 2010 Satoshi Nakamoto
-// Copyright (c) 2009-2014 The Bitcoin Core developers
+// Copyright (c) 2014-2015 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "chainparamsbase.h"
+#ifndef BITCOIN_CHAINPARAMSBASE_H
+#define BITCOIN_CHAINPARAMSBASE_H
 
-#include "util.h"
-
-#include <assert.h>
-
-/**
- * Main network
- */
-class CBaseMainParams : public CBaseChainParams
-{
-public:
-    CBaseMainParams()
-    {
-        nRPCPort = 7932;
-    }
-};
-static CBaseMainParams mainParams;
+#include <string>
+#include <vector>
 
 /**
- * Testnet (v3)
+ * CBaseChainParams defines the base parameters (shared between dash-cli and dashd)
+ * of a given instance of the Dash system.
  */
-class CBaseTestNetParams : public CBaseMainParams
+class CBaseChainParams
 {
 public:
-    CBaseTestNetParams()
-    {
-        nRPCPort = 17932;
-        strDataDir = "testnet4";
-    }
-};
-static CBaseTestNetParams testNetParams;
+    /** BIP70 chain name strings (main, test or regtest) */
+    static const std::string MAIN;
+    static const std::string TESTNET;
+    static const std::string REGTEST;
 
-/*
- * Regression test
+    const std::string& DataDir() const { return strDataDir; }
+    int RPCPort() const { return nRPCPort; }
+
+protected:
+    CBaseChainParams() {}
+
+    int nRPCPort;
+    std::string strDataDir;
+};
+
+/**
+ * Append the help messages for the chainparams options to the
+ * parameter string.
  */
-class CBaseRegTestParams : public CBaseTestNetParams
-{
-public:
-    CBaseRegTestParams()
-    {
-        strDataDir = "regtest";
-    }
-};
-static CBaseRegTestParams regTestParams;
+void AppendParamsHelpMessages(std::string& strUsage, bool debugHelp=true);
 
-/*
- * Unit test
+/**
+ * Return the currently selected parameters. This won't change after app
+ * startup, except for unit tests.
  */
-class CBaseUnitTestParams : public CBaseMainParams
-{
-public:
-    CBaseUnitTestParams()
-    {
-        strDataDir = "unittest";
-    }
-};
-static CBaseUnitTestParams unitTestParams;
+const CBaseChainParams& BaseParams();
 
-static CBaseChainParams* pCurrentBaseParams = 0;
+CBaseChainParams& BaseParams(const std::string& chain);
 
-const CBaseChainParams& BaseParams()
-{
-    assert(pCurrentBaseParams);
-    return *pCurrentBaseParams;
-}
+/** Sets the params returned by Params() to those for the given network. */
+void SelectBaseParams(const std::string& chain);
 
-void SelectBaseParams(CBaseChainParams::Network network)
-{
-    switch (network) {
-    case CBaseChainParams::MAIN:
-        pCurrentBaseParams = &mainParams;
-        break;
-    case CBaseChainParams::TESTNET:
-        pCurrentBaseParams = &testNetParams;
-        break;
-    case CBaseChainParams::REGTEST:
-        pCurrentBaseParams = &regTestParams;
-        break;
-    default:
-        assert(false && "Unimplemented network");
-        return;
-    }
-}
+/**
+ * Looks for -regtest, -testnet and returns the appropriate BIP70 chain name.
+ * @return CBaseChainParams::MAX_NETWORK_TYPES if an invalid combination is given. CBaseChainParams::MAIN by default.
+ */
+std::string ChainNameFromCommandLine();
 
-CBaseChainParams::Network NetworkIdFromCommandLine()
-{
-    bool fRegTest = GetBoolArg("-regtest", false);
-    bool fTestNet = GetBoolArg("-testnet", false);
+/**
+ * Return true if SelectBaseParamsFromCommandLine() has been called to select
+ * a network.
+ */
+bool AreBaseParamsConfigured();
 
-    if (fTestNet && fRegTest)
-        return CBaseChainParams::MAX_NETWORK_TYPES;
-    if (fRegTest)
-        return CBaseChainParams::REGTEST;
-    if (fTestNet)
-        return CBaseChainParams::TESTNET;
-    return CBaseChainParams::MAIN;
-}
-
-bool SelectBaseParamsFromCommandLine()
-{
-    CBaseChainParams::Network network = NetworkIdFromCommandLine();
-    if (network == CBaseChainParams::MAX_NETWORK_TYPES)
-        return false;
-
-    SelectBaseParams(network);
-    return true;
-}
-
-bool AreBaseParamsConfigured()
-{
-    return pCurrentBaseParams != NULL;
-}
+#endif // BITCOIN_CHAINPARAMSBASE_H
